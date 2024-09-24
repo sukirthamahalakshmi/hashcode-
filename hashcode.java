212c0c2bf5f58eb015dae1f1bab78f6aff7010d2
sukirthamahalakshmi <143057131+sukirthamahalakshmi@users.noreply.github.com>
2024-09-24 09:19:26 +0530

    Create hashcode.java

---
+++ hashcode.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Str {
+
+	public static void main(String[] args) {
+		   	
+		  String s1= new String("hai");
+		  System.out.println(s1.hashCode());
+		s1=  s1.concat("hello");
+		  System.out.println(s1);
+		  System.out.println(s1.hashCode());
+ 	   
+
+}}
